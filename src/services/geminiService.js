eb09fabcb619a685821a962470c1c1364b32b22c
Nadimahamad Naikwade <34714804+nadimahamad@users.noreply.github.com>
2024-08-26 10:35:24 +0530

    Update geminiService.js

--- src/services/geminiService.js
+++ src/services/geminiService.js
@@ -49,24 +49,3 @@ export const identifyPlant = async (imageBase64) => {
 };
 
 export const translateText = async (text, targetLanguage) => {
-  // const url = `https://translation.googleapis.com/language/translate/v2?key="AIzaSyBeOiIzclBi1Cst75Oj1j017Z94SpXmP68"`;
-  
-  // try {
-  //   const response = await fetch(url, {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify({
-  //       q: text,
-  //       target: targetLanguage,
-  //     }),
-  //   });
-
-  //   const data = await response.json();
-  //   return data.data.translations[0].translatedText;
-  // } catch (error) {
-  //   console.error('Translation error:', error);
-  //   throw error;
-  // }
-};
