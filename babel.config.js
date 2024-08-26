a37385c9d2180e4a0d89d39ab81a24a18eb3a3a4
Nadimahamad Naikwade <34714804+nadimahamad@users.noreply.github.com>
2024-08-26 13:27:24 +0530

    Add files via upload

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    '@babel/plugin-proposal-export-namespace-from',
+    'react-native-reanimated/plugin',
+  ],
+
+};
